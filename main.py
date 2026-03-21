b24864db3ea79cc78d1e38e2a7e596a176c418b6
orhanfidans21-cmd <orhanfidans21@gmail.com>
2026-03-21 17:59:28 +0300

    Update Telegram bot message content and structure

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import time
+import os
 
+# Senin bilgilerini buraya tırnak içinde yazdım
 TOKEN = "8722918294:AAEcpreA9fn9qtVXz5YBBAC7M19jo8-KUTE"
 CHAT_ID = "1157525263"
 
 def mesaj_at():
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text=Abe_test_basarili!"
+    text = "Abe_sonunda_baglandim_analiz_hazir!"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={text}"
     try:
         requests.get(url)
         print("Mesaj gönderildi!")
@@ -13,6 +16,10 @@ def mesaj_at():
         print("Hata!")
 
 if __name__ == "__main__":
+    # Render'ın "Port" hatası vermemesi için bu 2 satır şart:
+    print("Bot baslatiliyor...")
+    mesaj_at() # Çalışır çalışmaz ilk mesajı atar
+    
     while True:
-        mesaj_at()
+        # Botun kapanmaması için döngüde tutuyoruz
         time.sleep(60)
