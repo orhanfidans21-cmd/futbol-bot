57881ebfff6ced746b63a451bf9b006499707d65
orhanfidans21-cmd <orhanfidans21@gmail.com>
2026-03-21 17:53:42 +0300

    Refactor Telegram message sending function

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import requests
 import time
 
-# Buradaki bilgileri resimdeki gibi (tırnak içinde) doldur
-TOKEN = "8722918294:AAEcpreA9fn9qtVXz5YBBAC7M19jo8-KUTE" # Kendi Token'ını yaz
-CHAT_ID = "1157525263"        # Kendi ID'ni yaz
+# Bilgilerini buraya tırnak içinde yaz
+TOKEN = "8122918294:AAEcpreA9fn9qtVXz5YBBAC7M19jo8-KUTE"
+CHAT_ID = "1157525263"
 
-def test_mesaji_gonder():
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text=Abe_mesaj_geldi_mi?"
+def test_mesaji():
+    print("Telegram'a mesaj gönderiliyor...")
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text=Abe_test_basarili_baglanti_kuruldu!"
     try:
-        requests.get(url)
-        print("Telegram'a mesaj gönderildi!")
+        r = requests.get(url)
+        print(f"Sonuç: {r.status_code}")
     except Exception as e:
-        print(f"Hata oluştu: {e}")
+        print(f"Hata: {e}")
 
-# Bot her 1 dakikada bir sana mesaj atacak
-while True:
-    test_mesaji_gonder()
-    time.sleep(60)
+# Botu başlat ve her 1 dakikada bir mesaj at
+if __name__ == "__main__":
+    while True:
+        test_mesaji()
+        time.sleep(60)
